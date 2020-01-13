cfa328faeacdf3d37456b5a286b054cf56d02045
Clement Calmels <clement.calmels@free.fr>
2020-01-13 17:33:37 +0100

    add login routes

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require('express');
 const session = require('express-session');
 const uuid = require('uuid/v4');
 const file_store = require('session-file-store')(session);
+const body_parser = require('body-parser');
 
 const app = express();
+
+app.use(body_parser.urlencoded({ extended: false }));
+app.use(body_parser.json());
 
 app.use(session({
     genid: function(req) {
@@ -18,6 +22,18 @@ app.use(session({
 
 app.get('/', function(req, res) {
     res.send('Home page with id: ' + req.sessionID);
+});
+
+// login routes
+app.get('/login', function(req, res) {
+    console.log('/login function ' + req.sessionID);
+    res.send(`This is the login page!`);
+});
+
+app.post('/login', function(req, res) {
+    console.log('/login post');
+    console.log(req.body);
+    res.send('posted on the login page');
 });
 
 app.listen(3000, function() {
