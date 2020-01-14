711519ef2488c7aa158e18122ec953c24aa9ef69
Clement Calmels <clement.calmels@free.fr>
2020-01-14 14:22:04 +0100

    add passport

--- app.js
+++ app.js
@@ -3,6 +3,29 @@ const session = require('express-session');
 const uuid = require('uuid/v4');
 const file_store = require('session-file-store')(session);
 const body_parser = require('body-parser');
+const passport = require('passport');
+const passport_local = require('passport-local').Strategy;
+
+const users = [
+    { id: '123', email: 'foo@bar.com', password: 'password' }
+];
+
+passport.use(new passport_local(
+    { usernameField: 'email' },
+    function(email, password, done) {
+	console.log('in local strategy');
+
+	const user = users[0];
+	if (email === user.email && password === user.password) {
+	    console.log('local strategy returns true');
+	    return done(null, user);
+	}
+    }));
+
+passport.serializeUser(function(user, done) {
+    console.log('in serializeUser');
+    done(null, user.id);
+});
 
 const app = express();
 
@@ -20,6 +43,9 @@ app.use(session({
     saveUninitialized: true
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.get('/', function(req, res) {
     res.send('Home page with id: ' + req.sessionID);
 });
@@ -30,10 +56,22 @@ app.get('/login', function(req, res) {
     res.send(`This is the login page!`);
 });
 
-app.post('/login', function(req, res) {
+app.post('/login', function(req, res, next) {
     console.log('/login post');
-    console.log(req.body);
-    res.send('posted on the login page');
+
+    passport.authenticate('local', function(err, user, info) {
+	console.log('in authenticate for '
+		    + JSON.stringify(req.session.user)
+		    + ' with '
+		    + JSON.stringify(req.session.passport));
+	req.login(user, function(err) {
+	    console.log('in login for '
+			+ JSON.stringify(req.user)
+			+ ' with '
+			+ JSON.stringify(req.session.passport));
+	    return res.send('you are authenticated');
+	})
+    })(req, res, next);
 });
 
 app.listen(3000, function() {
